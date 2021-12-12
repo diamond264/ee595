91b84d10d05c9d4d3c56209214d0e9ae8b822000
뛰는하마 <gbstinyhippo@gmail.com>
2021-12-13 02:51:40 +0900

    compensate gravity

--- arduino/final/final.ino
+++ arduino/final/final.ino
@@ -7,21 +7,12 @@ typedef struct LocationMeasurement {
 } LocationMeasurement;
 LocationMeasurement loc;
 
-typedef struct OrientationMeasurement {
-  byte flag = 0b00000000; // Refer to the "GATT Specification Supplement" for more detail
-  byte orientation[12]; // Byte-array used instead of float, to resolve 4-byte aligning issue
-} OrientationMeasurement;
-OrientationMeasurement orm;
-
 BLEService YoutubeService("1809");
 
 BLECharacteristic LocationMeasurementChar("2A1C", BLERead | BLENotify, sizeof(loc), true);
 byte cccd_value[2]= {0b00000000, 0b00000010}; 
 BLEDescriptor cccd("2902", cccd_value, 2); // UUID of CCCD is 2902
 
-BLECharacteristic OrientationMeasurementChar("2A1E", BLERead | BLENotify, sizeof(orm), true);
-byte cccd_value_2[2]= {0b00000000, 0b00000001};
-BLEDescriptor cccd_2("2902", cccd_value_2, 2);
 
 void blePeripheralConnectHandler(BLEDevice central) {
   // central connected event handler
@@ -48,10 +39,10 @@ int n;
 float ax, ay, az;
 float ax_offset, ay_offset, az_offset;
 float ax_temp, ay_temp, az_temp;
-const float rotate_coeff = 1000000 * (2*PI/360);
+const float rotate_coeff = 1000000.0 * (2.0*PI/360.0);
 
 float accAngleX = 0, accAngleY = 0, gyroAngleX = 0, gyroAngleY = 0, gyroAngleZ = 0;
-float roll, pitch, yaw; 
+float roll = 0, pitch = 0, yaw = 0; 
 
 bool start_flag;
 
@@ -80,11 +71,8 @@ void setup() {
   
   // Characteristics with "Indicate" property has a CCCD descriptor already installed. To change the value, the only possible way is to completely replace it
   LocationMeasurementChar.descriptor("2902") = cccd;
-  // Characteristics with "Notify" property has a CCCD descriptor already installed. To change the value, the only possible way is to completely replace it
-  OrientationMeasurementChar.descriptor("2902") = cccd_2;
 
   YoutubeService.addCharacteristic(LocationMeasurementChar); // Mandatory. Refer to the "Health Thermometer Service" document for more detail
-  YoutubeService.addCharacteristic(OrientationMeasurementChar); // Optional. Refer to the "Health Thermometer Service" document for more detail
 
   BLE.addService(YoutubeService);
   BLE.setAdvertisedService(YoutubeService);
@@ -135,63 +123,56 @@ void loop() {
       microTime_delta = microTime - microTime_old;
       microTime_old = microTime;
 
+      
+      IMU.readAccelerationGyroMultiple(ax, ay, az, gx, gy, gz, n);
+
       if(start_flag){
         start_flag = false;
         continue;
       }
-      
-      IMU.readAccelerationGyroMultiple(ax, ay, az, gx, gy, gz, n);
 
-      gx = gx - 2.6;
-      gy = gy + 14;
+      gx = gx - 2.5;
+      gy = gy + 13.25;
       gz = gz - 0.75;
       
       ax = -ax;
       gx = -gx;
+    
+      float thres = gx*gx + gy*gy + gz*gz;
+      if (thres < 200)
+        { 
+          gy = 0;
+          gx = 0;
+          gz = 0;
+        }
+        
+      yaw =  yaw + gz * microTime_delta / rotate_coeff;
+      roll = roll + gx * microTime_delta / rotate_coeff;
+      pitch = pitch + gy * microTime_delta / rotate_coeff;
 
-      ax -= ax_offset;
-      ay -= ay_offset;
-      az -= az_offset;
-      
-      accAngleX = (atan(ay / sqrt(pow(ax, 2) + pow(az, 2))) * 180 / PI);
-      accAngleY = (atan(ax / sqrt(pow(ay, 2) + pow(az, 2))) * 180 / PI);
+//      pitch, yaw, roll
+//      float rotation_matrix[3][3] = {{cos(pitch)*cos(yaw), cos(pitch)*sin(yaw)*sin(roll)-sin(pitch)*cos(roll), cos(pitch)*sin(yaw)*cos(roll) + sin(pitch)*sin(roll)},
+//                                      {sin(pitch)*cos(yaw), sin(pitch)*sin(yaw)*sin(roll)-cos(pitch)*cos(roll), sin(pitch)*sin(yaw)*cos(roll) + cos(pitch)*sin(roll)},
+//                                         {-sin(yaw), cos(yaw)*sin(roll), cos(yaw)*cos(roll)}};
 
-      gyroAngleX = gyroAngleX + gx * microTime_delta / rotate_coeff;
-      gyroAngleY = gyroAngleY + gy * microTime_delta / rotate_coeff;
-      
-      
-    
-    //  rotate_x = rotate_x + gx * microTime_delta / rotate_coeff;
-    //  rotate_y = rotate_y + gy * microTime_delta / rotate_coeff;
-    //  rotate_z = rotate_z + gz * microTime_delta / rotate_coeff;
-    //
-    //  // Remove roll
-    //  ax = -ax;
-    //  ax_temp = ax;
-    //  ay_temp = ay * cos(rotate_x) - az * sin(rotate_x);
-    //  az_temp = ay * sin(rotate_x) + az * cos(rotate_x);
-    //  ax = ax_temp;
-    //  ay = ay_temp;
-    //  az = az_temp;
-    //  
-    //  // Remove pitch
-    //  ax_temp = ax * cos(rotate_y) + az * sin(rotate_y);
-    //  ay_temp = ay;
-    //  az_temp = - ax * sin(rotate_y) + az * cos(rotate_y);
-    //  ax = ax_temp;
-    //  ay = ay_temp;
-    //  az = az_temp;
-    //
-    //  // Remove yaw
-    //  ax_temp = ax * cos(rotate_z) - ay * sin(rotate_z);
-    //  ay_temp = ax * sin(rotate_z) + ay * cos(rotate_z);
-    //  az_temp = az;
-    //  ax = ax_temp;
-    //  ay = ay_temp;
-    //  az = az_temp;
-    
-      
-    
+     float d2r = PI/180.0;
+     float rotated_gravity[3] = {-sin(pitch*d2r/56), cos(pitch*d2r/56)*sin(roll*d2r/56), cos(pitch*d2r/56)*cos(roll*d2r/56)};
+
+     ax -= rotated_gravity[0];
+     ay -= rotated_gravity[1];
+     az -= rotated_gravity[2];
+
+     if (thres < 200)
+        { 
+          ax_offset = ax;
+          ay_offset = ay;
+          az_offset = az;
+        }
+
+        ax -= ax_offset;
+        ay -= ay_offset;
+        az -= az_offset;
+
       vx_add = 4.9 * ax * microTime_delta / 1000000.0;
       vy_add = 4.9 * ay * microTime_delta / 1000000.0;
       vz_add = 4.9 * az * microTime_delta / 1000000.0;
@@ -203,15 +184,9 @@ void loop() {
       vx = pos_vx + vx_add;
       vy = pos_vy + vy_add;
       vz = pos_vz + vz_add;
-    
-      float thres = gx*gx + gy*gy + gz*gz;
+
       if (thres < 200)
-        {
-          
-          ax_offset = ax_offset + ax;
-          ay_offset = ay_offset + ay;
-          az_offset = az_offset + az;
-          
+        { 
           pos_vx = 0;
           pos_vy = 0;
           pos_vz = 0;
@@ -219,39 +194,11 @@ void loop() {
           vx = 0;
           vy = 0;
           vz = 0;
-    
-          rotate_x = 0;
-          rotate_y = 0;
-          rotate_z = 0;
-
-          gz = 0;
         }
-      else if (thres > 30000)
-      {
-          pos_vx = 0;
-          pos_vy = 0;
-          pos_vz = 0;
-    
-          vx = 0;
-          vy = 0;
-          vz = 0;
-    
-          rotate_x = 0;
-          rotate_y = 0;
-          rotate_z = 0;
-      }
-
-      yaw =  yaw + gz * microTime_delta / rotate_coeff;
-      roll = 0.96 * gyroAngleX + 0.04 * accAngleX;
-      pitch = 0.96 * gyroAngleY + 0.04 * accAngleY;
     
       x = x + pos_vx * microTime_delta / 1000000.0;
       y = y + pos_vy * microTime_delta / 1000000.0;
       z = z + pos_vz * microTime_delta / 1000000.0;
-
-      //*(float*)&orm.orientation[0] = (float)yaw / 56;
-      //*(float*)&orm.orientation[4] = (float)roll / 56;
-      //*(float*)&orm.orientation[8] = (float)pitch / 56;
 
       *(float*)&loc.location[0] = (float)x*100;
       *(float*)&loc.location[4] = (float)y*100;
@@ -261,16 +208,25 @@ void loop() {
       *(float*)&loc.location[20] = (float)roll / 56;
 
       LocationMeasurementChar.writeValue((uint8_t*)&loc, sizeof(loc));
-      // OrientationMeasurementChar.writeValue((uint8_t*)&orm, sizeof(orm));
   
   
 //    Serial.print(yaw/56);
 //    Serial.print('\t');
 //    Serial.print(roll/56);
 //    Serial.print('\t');
-//    Serial.println(pitch/56);
-  
-      // Serial.println(y*100);
+//    Serial.print(pitch/56);
+//    Serial.print('\t');
+//    Serial.print(ax);
+//    Serial.print('\t');
+//    Serial.print(ay);
+//    Serial.print('\t');
+//    Serial.println(az);
+
+    Serial.print(x*100);
+    Serial.print('\t');
+    Serial.print(y*100);
+    Serial.print('\t');
+    Serial.println(z*100);
     }
   }else {
     digitalWrite(22, LOW);
